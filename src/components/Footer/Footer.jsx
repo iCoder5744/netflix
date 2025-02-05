e18e7925912be0ce74be7d3a200ebb81a5ee10b3
Shivam Yadav <shivam.coder01@gmail.com>
2025-02-06 04:30:58 +0530

    Adding TitleCards and maked a footer bar

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,41 @@
 import React from 'react'
 import './Footer.css'
+import youtube_icon from '../../assets/youtube_icon.png'
+import twitter_icon from '../../assets/twitter_icon.png'
+import instagram_icon from '../../assets/instagram_icon.png'
+import facebook_icon from '../../assets/facebook_icon.png'
+
 
 const Footer = () => {
   return (
     <div className='footer'>
-        Footer
+      <div className='footer-icons'>
+        <img src={facebook_icon} alt="" />
+        <img src={instagram_icon} alt="" />
+        <img src={twitter_icon} alt="" />
+        <img src={youtube_icon} alt="" />
+      </div>
+      <div className="list">
+      <ul>
+        <li>Audio Description</li>
+        <li>Help Center</li>
+        <li>Gift Cards</li>
+        <li>Media Center</li>
+        <li>Invester Relations</li>
+      </ul>
+      <ul>        
+        <li>Jobs</li>
+        <li>Terms of Use</li>
+        <li>Privacy</li>
+        <li>legal Notices</li>
+      </ul>
+      <ul>
+        <li>Cookie Preferences</li>
+        <li>Corporate Information</li>
+        <li>Contact Us</li>
+      </ul>
+      </div>
+      <p className="copyright-text">Copyright By Shivam Yadav</p>
     </div>
   )
 }
--- src/components/TitleCards/TitleCarrds.jsx
+++ src/components/TitleCards/TitleCarrds.jsx
@@ -2,7 +2,7 @@ import React, { useRef } from "react";
 import "./TitleCards.css";
 import cards_data from "../../assets/cards/Cards_data";
 
-const TitleCards = () => {
+const TitleCards = ({title, category}) => {
   const cardsRef = useRef(null);
 
   // Scroll the card list when the arrow button is clicked
@@ -14,7 +14,7 @@ const TitleCards = () => {
 
   return (
     <div className="title-cards">
-      <h2>Popular on Netflix</h2>
+      <h2>{title?title:"Popular on Netflix"}</h2>
       <div className="card-list" ref={cardsRef}>
         {cards_data.map((card, index) => (
           <div className="card" key={index}>
